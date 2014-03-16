d5291867a2508d580b8bf0e75e82f892ffac7ec2
yunbang13 <yunbang13@gmail.com>
2014-03-16 20:12:44 +0800

    perubahan header & footer, bg image & jata

--- footer.php
+++ footer.php
@@ -1,12 +1,11 @@
 <?php 
 	echo "<tr><td>";
 ?>
-  <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" height="39">
+  <table width="100%" border="0" cellpadding="0" cellspacing="0" align="center" height="40">
   <tr><td background="images/footer.jpg">
 		<div align="center"><font size="1">
-Hakcipta &copy; 2009 JEMAAH NAZIR DAN JAMINAN KUALITI (JNJK).<br>
-PENAFIAN : Kementerian Pelajaran Malaysia tidak akan bertanggungjawab ke atas sebarang kehilangan atau kerosakan<br> 
-yang diakibatkan oleh penggunaan maklumat yang dicapai daripada laman web ini.<br>
+Hakcipta &copy; 2014 CMMS<br>
+JABATAN PERDANA MENTERI<br>
         </font></div>
 </td></tr>
 </table>
--- header.php
+++ header.php
@@ -3,7 +3,7 @@
     <td colspan="4"valign="bottom">
 	<table width="978" border="0" cellspacing="0" cellpadding="0">
         <tr>
-          <td colspan="3"><img src="images/header.jpg"></td>
+          <td colspan="3"><img src="images/header_temp.png"></td>
         </tr>
       </table>
 	  </td>
