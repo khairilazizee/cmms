0f900fa99280b9daf64ca04569d0862530429da1
khairil azizee <khairil114@gmail.com>
2014-03-18 23:25:48 +0800

    inventori

---
+++ modules/Setup/list_inventori.php
@@ -0,0 +1,95 @@
+<?php
+
+include('include/function.php');
+$Mfunction = new fungsi();
+$limit = 25;
+
+if(!isset($_GET["limit"]))
+  $rowstart = 0;
+else
+  $rowstart = $_GET["limit"];
+  
+
+if($_POST['submit']){
+    $code = $_POST['txtSearchCode'];
+    $desc = mysql_real_escape_string($_POST['txtSearchDescription']);
+}
+
+if($_GET['delete']=="1"){
+    $iddelete = $_GET['iddelete'];
+    
+    $delete = "DELETE FROM tbl_inventori WHERE id='$iddelete'";
+    sql_query($delete,$dbi);
+    
+    pageredirect("mainpage.php?module=Setup&task=list_inventori");
+}
+
+?>
+<!-- <div style="float:left;">
+    <table>
+        <form name="frmcariansistem">
+        <tr>
+            <td>Jenis Inventori</td>
+            <td>:</td>
+            <td>
+                <input type="hidden" name="CarianSistem" id="CarianSistem" value="<?php echo $kumpsistem; ?>" />
+                <select name="txtSystemGroup" id="txtSystemGroup" onchange="return carisistem();">
+                    <option value="">- SEMUA -</option>
+                    <?php
+                        $sql = "SELECT sg_id, sg_desc FROM system_group ORDER BY sg_id";
+                        $res = mysql_query($sql,$dbi);
+                        while($sgdata = mysql_fetch_array($res)){
+                            $sgid = $sgdata['sg_id'];
+                            $sgdesc = $sgdata['sg_desc'];
+
+                            echo "<option value='$sgid' "; 
+                            if($sgid==$kumpsistem)
+                                echo "selected "; 
+                            echo ">$sgdesc</option>";
+                        }
+                    ?>
+                </select>
+            </td>
+        </tr>
+        </form>
+    </table>
+</div> -->
+<div style="text-align:right;font-weight:bold;"><a href="mainpage.php?module=Setup&task=setup_inventori">Tambah<img src="images/admin/btn_add.gif"></a></div><br>
+<table width="100%" cellspacing="1" cellpadding="4" align="center" class="table">
+    <tr>
+        <td style="font-weight:bold;" class="formheader" colspan="5">Senarai Inventori</td>
+    </tr>
+    <tr>
+        <th width="5">Bil</th>
+        <th>Jenis Barangan</th>
+        <th width="100">Jumlah</th>
+        <th width="80">Tindakan</th>
+    </tr>
+    <?php
+        $sqlbarangan = "SELECT id, nama_barangan, jum_barangan FROM tbl_inventori ";
+        $sqlbaranganfull = $sqlbarangan." LIMIT $rowstart, $limit";
+        $resbarangan = mysql_query($sqlbarangan,$dbi);
+        $resbaranganfull = mysql_query($sqlbaranganfull,$dbi);
+        $cnt = $rowstart;
+        while($data = mysql_fetch_array($resbaranganfull)){
+            $cnt++;
+            $namaitem = $data['nama_barangan'];
+            $jumlah = $data['jum_barangan'];
+            $idinv = $data['id'];
+
+            echo "<tr bgcolor=\"$bgcolor\" onMouseOver=\"this.bgColor = '$hlcolor'\" onMouseOut =\"this.bgColor = '$bgcolor'\">\n";
+            echo "<td>$cnt</td>";
+            echo "<td>$namaitem</td>";
+            echo "<td>$jumlah</td>";
+            echo "<td align=\"center\">
+                <a href=\"mainpage.php?module=Setup&task=setup_inventori&inv=$idinv\"><img src=\"images/admin/btn_edit.gif\"/></a>&nbsp;&nbsp;<a href=\"mainpage.php?module=Setup&task=list_inventori&delete=1&iddelete=$idinv\" onClick=\"return confirm('Hapus Data?');\"><img src=\"images/admin/btn_delete.gif\"/></a>
+            </td>";
+            echo "</tr>";
+        }
+    ?>
+</table>
+<div style="text-align:center;">
+    <?php
+    print $Mfunction->page("?module=Setup&task=list_inventori", $limit, $rowstart, $numrows);
+    ?>
+</div>
---
+++ modules/Setup/setup_inventori.php
@@ -0,0 +1,58 @@
+<?php
+
+$invid = (int) mysql_real_escape_string($_REQUEST['inv']);
+
+if(isset($_POST['submit'])){
+	$namabarangan = mysql_real_escape_string($_POST['txtNamaBarangan']);
+	$jumlahbarangan = mysql_real_escape_string($_POST['txtJumlahBarangan']);
+	$idbarang = $_POST['inv'];
+	$flg = $_POST['flg'];
+
+	if($flg=="add"){
+		$qryinventori = "INSERT INTO tbl_inventori (nama_barangan,jum_barangan) VALUES ('$namabarangan','$jumlahbarangan')";
+	} elseif($flg=="edit"){
+		$qryinventori = "UPDATE tbl_inventori SET nama_barangan='$namabarangan', jum_barangan='$jumlahbarangan' WHERE id='$idbarang'";
+	}
+
+	mysql_query($qryinventori,$dbi);
+
+	pageredirect("mainpage.php?module=Setup&task=list_inventori");
+}
+
+$flg = "add";
+$sqlinventori = "SELECT id, nama_barangan, jum_barangan FROM tbl_inventori WHERE id='$invid'";
+// echo $sqlinventori;
+$resinventori = mysql_query($sqlinventori,$dbi);
+if($datainv = mysql_fetch_array($resinventori)){
+	$flg = "edit";
+	$namaitem = $datainv['nama_barangan'];
+	$jumlah = $datainv['jum_barangan'];
+	$idbarangan = $datainv['id'];
+}
+
+?>
+<form name="frmtask" method="POST" action="">
+<table width="100%" cellspacing="3" cellpadding="3" align="center" class="outerform">
+    <tr>
+        <td style="font-weight:bold;" class="formheader" colspan="3">Inventori</td>
+    </tr>
+    <tr>
+    	<td class="title" width="20%">Nama Barangan</td>
+    	<td class="title" width="2%">:</td>
+    	<td><input type="text" name="txtNamaBarangan" value="<?php echo $namaitem;?>" /></td>
+    </tr>
+    <tr>
+    	<td class="title">Jumlah Barangan</td>
+    	<td class="title">:</td>
+    	<td><input type="text" name="txtJumlahBarangan" size="10" value="<?php echo $jumlah;?>" /></td>
+    </tr>
+    <tr>
+        <td colspan="3">
+            <input type="hidden" name="inv" value="<?php echo $idbarangan;?>"/>
+            <input type="hidden" name="flg" value="<?php echo $flg;?>"/>
+            <input type="submit" value="Hantar" name="submit" class="button"/ onClick="return checkform();">
+            <input type="button" name="back" value="Kembali" onclick="location.href='mainpage.php?module=Setup&task=list_inventori'" class="button"/>
+        </td>
+    </tr> 
+</table>
+</form>
