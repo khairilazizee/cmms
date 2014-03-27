0f154ed2afbbd7ce32c9fc332df262a4dd685233
khairil azizee <khairil114@gmail.com>
2014-03-27 11:30:58 +0800

    blok statistik

--- blocks/block-Statistik.php
+++ blocks/block-Statistik.php
@@ -5,6 +5,17 @@ defined( '_UMPORTAL' ) or die( 'Akses tidak dibenarkan !' );
 include_once("mainfile.php");
 global $dbi;
 
-$content="<table width='200'>";
+$content="<table width='200' cellspacing='3' cellpadding='1' class='table'>";
+$sql = "SELECT ws_id, ws_desc FROM work_status ORDER BY ws_id";
+$res = mysql_query($sql,$dbi);
+while($data = mysql_fetch_array($res)){
+	$descworking = $data['ws_desc'];
+	$content.="<tr>";
+	$content.="<td style='background-color:transparent'>$descworking</td>";
+	$content.="<td style='background-color:transparent' align='center'>x</td>";
+	$content.="</tr>";
+}
+$content.="</table>";
+
 
 ?>
